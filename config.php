acbb89be737d0409063d2898c44f26101823e250
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-06-11 01:17:18 +0900

    feat-2 User Page and appropriate connection
    - Created User Page before accessing home page, and actual connection with database
    - Previous (Establishment of Database was done but not connection yet)

---
+++ config.php
@@ -0,0 +1,6 @@
+﻿<?
+    $host = "localhost";
+    $dbid = "db2023951086";                    // 자신의 DB ID
+    $dbpass = "youngmink517@gmail.com";                  // 자신의 DB Password
+    $dbname = "db2023951086";                  // 해당 프로젝트에서 사용하는 DB명
+?>
