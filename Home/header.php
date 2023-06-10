f07bc874de9b63fd6c485356e8e92ee69b30c37c
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-06-11 03:51:33 +0900

    HomePage
    feat-5
    Creation of basic structures of HomePage

---
+++ Home/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='ko'>
+<head>
+    <title>K-Ticket</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<form action="product_list.php" method="post">
+    <div class='navbar fixed'>
+        <div class='container'>
+            <a class='pull-left title' href="index.php">K-Ticket</a>
+            <ul class='pull-right'>
+                <li>
+                    <input type="text" name="search_keyword" placeholder="K-Ticket Search Engine">
+                </li>
+                <li><a href='product_list.php'>Event List</a></li>
+                <li><a href='product_form.php'>Event Registration</a></li>
+                <li><a href='buy_form.php'>Ticket Purchase</a></li>
+                <li><a href='buy_list.php'>Purchase List</a></li>
+                <li><a href="../login_form.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</form>
---
+++ Home/index.php
@@ -0,0 +1,27 @@
+<? include ("header.php"); ?>
+    <style>
+        .navbar {
+            z-index:999;
+        }
+        h1, p {
+            padding: 0px 30px 0px 30px;
+            text-align:center;
+        }
+        h1 {
+            font-weight:800;
+        }
+        .container {
+            position: relative;
+            background: rgba(255, 255, 255, .9);
+        }
+        .ref {
+            font-weight:200;
+            font-size:0.5em;
+        }
+    </style>
+    <div class='container'>
+        <p align="center"><img src="images/festival.jpg" width="60%"></p>
+        <h1>K-Ticket</h1>
+        <p>Welcome to Korea University's K-Ticket Website!</br><b>Purchase your tickets now!</b></p>
+        <p class="ref">The resources used on this website are strictly for learning purposes, and commercial use is restricted.
+<? include ("footer.php"); ?>
