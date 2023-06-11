b76b8947d52d61a5b41d4e6e5b424227d6831ac1
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-06-12 04:17:46 +0900

    Update product_modify.php
    Update same reason

--- Home/product_modify.php
+++ Home/product_modify.php
@@ -1,10 +1,10 @@
 <?php
+include "config.php";    // Database connection settings
+include "util.php";      // Utility functions
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-include "config.php";    // Database connection settings
-include "util.php";      // Utility functions
 
 $conn = dbconnect($host, $dbid, $dbpass, $dbname);
 
@@ -13,6 +13,7 @@ $manufacturer_id = $_POST['manufacturer_id'];
 $product_name = $_POST['product_name'];
 $product_desc = $_POST['product_desc'];
 $price = $_POST['price'];
+$date = $_POST['date'];
 
 // Check if a new image is uploaded
 if ($_FILES['image']['name'] !== '') {
@@ -23,21 +24,23 @@ if ($_FILES['image']['name'] !== '') {
     }
     // Upload the new image
     $image_path = handleImageUpload($_FILES['image']);
-    $query = "UPDATE product SET manufacturer_id = '$manufacturer_id', product_name = '$product_name', product_desc = '$product_desc', price = '$price', image_path = '$image_path' WHERE product_id = $product_id";
-    echo "New image uploaded. Query: $query"; // Debugging statement
+    $query = "UPDATE product SET manufacturer_id = ?, product_name = ?, product_desc = ?, price = ?, image_path = ?, date = ? WHERE product_id = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "isssssi", $manufacturer_id, $product_name, $product_desc, $price, $image_path, $date, $product_id);
 } else {
     // No new image uploaded, update other product details
-    $query = "UPDATE product SET manufacturer_id = '$manufacturer_id', product_name = '$product_name', product_desc = '$product_desc', price = '$price' WHERE product_id = $product_id";
-    echo "No new image uploaded. Query: $query"; // Debugging statement
+    $query = "UPDATE product SET manufacturer_id = ?, product_name = ?, product_desc = ?, price = ?, date = ? WHERE product_id = ?";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "issssi", $manufacturer_id, $product_name, $product_desc, $price, $date, $product_id);
 }
 
-$result = mysqli_query($conn, $query);
+$result = mysqli_stmt_execute($stmt);
 
 if ($result) {
     // Redirect to product_list.php
     header("Location: product_list.php");
     exit;
 } else {
-    die('Failed to add the product: ' . mysqli_error($conn));
+    die('Failed to update the product: ' . mysqli_error($conn));
 }
 ?>
