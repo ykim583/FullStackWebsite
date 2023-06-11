62babca464f26eec3720a94262150f0957a292df
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-06-12 04:19:30 +0900

    Update product_delete.php
    - delete all images when event delete
    - delete product-id, hence, associated seat information also delete

--- Home/product_delete.php
+++ Home/product_delete.php
@@ -1,28 +1,46 @@
 <?php
-include "config.php";    //데이터베이스 연결 설정파일
-include "util.php";      //유틸 함수
+include "config.php";    // 데이터베이스 연결 설정파일
+include "util.php";      // 유틸 함수
 
 $conn = dbconnect($host, $dbid, $dbpass, $dbname);
 
 $product_id = $_GET['product_id'];
 
-$check_query = "SELECT product_id FROM buy_item WHERE product_id = $product_id";
-$check_result = mysqli_query($conn, $check_query);
+// Retrieve the image path from the database
+$image_query = "SELECT image_path FROM product WHERE product_id = $product_id";
+$image_result = mysqli_query($conn, $image_query);
+$image_row = mysqli_fetch_assoc($image_result);
+$image_path = $image_row['image_path'];
 
-if (mysqli_num_rows($check_result) > 0) {
-    s_msg('The product cannot be deleted as it has already been ordered.');
+// Delete the associated seat records
+$delete_seat_query = "DELETE FROM seat WHERE product_id = $product_id";
+$delete_seat_result = mysqli_query($conn, $delete_seat_query);
+
+if (!$delete_seat_result) {
+    msg('Error deleting associated seat records: ' . mysqli_error($conn));
     echo "<meta http-equiv='refresh' content='0;url=product_list.php'>";
-} else {
-    $delete_query = "DELETE FROM product WHERE product_id = $product_id";
-    $result = mysqli_query($conn, $delete_query);
-
-    if (!$result) {
-        msg('Query Error: ' . mysqli_error($conn));
-    } else {
-        s_msg('The product has been successfully deleted.');
-        echo "<meta http-equiv='refresh' content='0;url=product_list.php'>";
-    }
+    exit;
 }
+
+// Delete the product from the product table
+$delete_product_query = "DELETE FROM product WHERE product_id = $product_id";
+$delete_product_result = mysqli_query($conn, $delete_product_query);
+
+if (!$delete_product_result) {
+    msg('Error deleting the product: ' . mysqli_error($conn));
+    echo "<meta http-equiv='refresh' content='0;url=product_list.php'>";
+    exit;
+}
+
+// Delete the image file from the server
+if (!empty($image_path) && file_exists($image_path)) {
+    unlink($image_path);
+}
+
+s_msg('The product has been successfully deleted.');
+echo "<meta http-equiv='refresh' content='0;url=product_list.php'>";
+
 ?>
+
 
 
