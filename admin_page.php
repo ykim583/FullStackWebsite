98b0f6077f347a54a649dbb7d143f87d24ed38f7
Youngmin Kim <100251600+ykim583@users.noreply.github.com>
2023-06-14 23:54:30 +0900

    Update admin_page.php
    For now on, admin page will have same functionality as users.
    Need to sort out the main page first before editing admin page.

--- admin_page.php
+++ admin_page.php
@@ -30,8 +30,7 @@ if(!isset($_SESSION['admin_name'])){
       <h3>hi, <span>admin</span></h3>
       <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
       <p>this is an admin page</p>
-      <a href="login_form.php" class="btn">login</a>
-      <a href="register_form.php" class="btn">register</a>
+      <a href="Home/index.php" class="btn">Home</a>
       <a href="logout.php" class="btn">logout</a>
    </div>
 
